d6d55f2d88245cf1d95ef4ac5d915e3d019fcbb1
keneth/217 <kipyegonkeneth03@gmail.com>
2025-02-20 11:03:42 +0300

    create screens,fetch,filter and hymn details

---
+++ app/src/main/java/com/keneth/hymnbook/TopBar.kt
@@ -0,0 +1,49 @@
+package com.keneth.hymnbook
+
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(scope: CoroutineScope, drawerState: DrawerState, title: String, onBottomSheetOpen: () -> Unit) {
+    CenterAlignedTopAppBar(
+        title = { Text(title, maxLines = 1, overflow = TextOverflow.Ellipsis) },
+        navigationIcon = {
+            IconButton(onClick = {
+                scope.launch {
+                    if (drawerState.isClosed) drawerState.open() else drawerState.close()
+                }
+            }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+
+            }
+        },
+        actions = {
+            IconButton(onClick = onBottomSheetOpen) {
+                Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Open Bottom Modal Sheet")
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
+}

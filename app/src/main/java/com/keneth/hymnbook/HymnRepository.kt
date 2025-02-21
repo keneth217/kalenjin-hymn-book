3022eaecbec8f4f78c384e5db5d264643d5d90e1
KENETH217 <kipyegonkeneth03@gmail.com>
2025-02-21 10:10:29 +0300

    add songs and navigation's

--- app/src/main/java/com/keneth/hymnbook/HymnRepository.kt
+++ app/src/main/java/com/keneth/hymnbook/HymnRepository.kt
@@ -73,52 +73,165 @@ object HymnRepository {
 
         hymns.add(
             Hymn(
-                number = 7, title = "Birechik ab kipsengwet", isFavourite = false, stanzas = listOf(
-                    "Birechik ab kipsengwet...", "Kokwetab ng'wony...", "Konyorunet ne mi..."
-                ), chorus = "Chorus: Kipsengwet che chang"
-            )
-        )
-
-        hymns.add(
-            Hymn(
-                number = 8, title = "Kokwo ne kimie", isFavourite = false, stanzas = listOf(
-                    "Kokwo ne kimie...", "Kigero chebo ng'wony...", "Konyitab kokwet..."
-                ), chorus = "Chorus: Kimie kamukta"
-            )
-        )
-
-        hymns.add(
-            Hymn(
-                number = 9, title = "Ng'ung'u chebo lugei", isFavourite = false, stanzas = listOf(
-                    "Ng'ung'u chebo lugei...", "Kokwetab keringet...", "Konyorunet ab kosir..."
-                ), chorus = "Chorus: Lugei che chang"
-            )
-        )
-
-        hymns.add(
-            Hymn(
-                number = 10, title = "Kipsengwet ab kokwet", isFavourite = false, stanzas = listOf(
-                    "Kipsengwet ab kokwet...", "Kigero chebo ng'wony...", "Kokwo ne birechik..."
+                number = 7, title = "Tilil! Tilil! Tilil!",
+                isFavourite = false, stanzas = listOf(
+                    "Tilil! TiliI! Tilil! Jehovah Kiptaiyat!\n" +
+                            "Inendet ko TiIiI - mami ne kiroge.\n" +
+                            "Kwanda ak ko Werit, ak ko Tamirmiriet,\n" +
+                            "2. Toror kot Inendet - otei bikab iman;\n" +
+                            "Ak ma1aika-isiek, kolosu agichek.\n" +
+                            "Kimitei koboch keny, ak komitei nguno;\n" +
+                            "Ak mitei koi'geny - Ine Kwandanyo.",
+                    "TiIiI kot Jehovah! Matinyei mesundei.\n" +
+                            "Mageerei Inendet bik che bo tengekto.\n" +
+                            "Bo-unindet Ine, ak Kamukta-indet.\n" +
+                            "Nenyi kimnatet. ak torornatet,.",
+                    "Tilil! Tilil! Tilil! I toror Inyege\n" +
+                            "I Ya-indet, 1nye, ne bo tuguk tugul.\n" +
+                            "Nguno, karonani. ketieni kilosun\n" +
+                            "Inye, Jehovah, Kamukta-indet. "
                 ), chorus = ""
             )
         )
+
         hymns.add(
             Hymn(
-                number = 11, title = "Yeiyo ne mi Jehova", isFavourite = false, stanzas = listOf(
-                    "Yeiyo ne mi Jehova...",
-                    "Kokwetab ng'wony chichi...",
-                    "Konyorunet ab keringet..."
-                ), chorus = "Chorus: Yeiyo ne mi kamukta"
+                number = 8, title = "Tun nyone Jesu ngwony kogeny",
+                isFavourite = false, stanzas = listOf(
+                    "Tun nyonei Jesu ng'ony kogeny\n" +
+                            "Nyo kolaitorane tugul;\n" +
+                            "Si kong'any bik tu-gul inne\n" +
+                            "Kole Kiptaiyat Inendet.", "2. Osoru bik che lusyotin\n" +
+                            "Kobwanji Laitoriandennyo.\n" +
+                            "Kasei kabe-ru-ret ne o\n" +
+                            "Bik che kisoru eng' me-et.",
+                    "'Ngotesak laitornatennyi\n" +
+                            "Eng' mugulelwekyok tugul.\n" +
+                            "Ongemwaitaa-te ng'a-lechu\n" +
+                            "Kogas emotinwek tugul.\n",
+                    "Kisa-e Jesu kotugul\n" +
+                            "Ak kitorori Kainennyi.\n" +
+                            "Nyoljin Ine-ndet tee-gisto\n" +
+                            "Ine ne o kosiir tugul.",
+                    "5. Obwa ketien eng' karemet,\n" +
+                            "Kiborjin bik katiaknatet:\n" +
+                            "Si koyom bik tugul eng' ng'ony\n" +
+                            "Kalosunetab Kiptaiyat."
+                ), chorus = ""
+            )
+        )
+
+        hymns.add(
+            Hymn(
+                number = 9, title = "Jehovah Irimet ne kim",
+                isFavourite = false,
+                stanzas = listOf(
+                    "Jehovah irimet ne kim\n" +
+                            "Ne mwechin bik the 'yanotin;\n" +
+                            "Ne toretech eng' maranet\n" +
+                            "Ye kinyalildosi mising'.\n" +
+                            "Amu cheng'ei bunyot\n" +
+                            "Oret ne ng'emenech;\n" +
+                            "0 kot ng'omnatennyi,\n" +
+                            "Karikyik ko wechet;\n" +
+                            "Ki ng'alindet koboch eng' tai.",
+                    " Ngot kimang'u kimnatennyo.\n" +
+                            "Koek anyun buch boriennyo;\n" +
+                            "Ago mi kobotech Ne Kim\n" +
+                            "Ne kigoyokwech Jehovah.\n" +
+                            "ilen ng'o Inendet:\n" +
+                            "Kristo Jesu Ine.\n" +
+                            "lnendet lnegei\n" +
+                            "Ne bo-u lugosiek.\n" +
+                            "Mami age ne 'terterwech.",
+                    "3. Agot ngo nyiitat ng'onyuni\n" +
+                            "Kimnatosiekab Setani,\n" +
+                            "Ko magiywei am u kicham\n" +
+                            "Kiptaiyandet koterterwech.\n" +
+                            "Magitoptobeni\n" +
+                            "Eng' bunyot o-indet;\n" +
+                            "Ma kiy neregekyik.\n" +
+                            "Amu wendi me-et.\n" +
+                            "Iterterisie' Imanit."
+                ), chorus = ""
+            )
+        )
+
+        hymns.add(
+            Hymn(
+                number = 10,
+                title = "Nyo inye Kaberurindet", isFavourite = false,
+                stanzas = listOf(
+                    "Nyo, lnye, Kaberurindet\n" +
+                            "Atieni mie-mdang'ung'.\n" +
+                            "Alosun amu chamnyeng'ung'\n" +
+                            "Ne irirege biguk.\n" +
+                            "     'Neta tiendab boibo-indang'ung'\n" +
+                            "     Ne tieni che mi barak.\n" +
+                            "     Alosun, Jesu. Ruandennyu;\n" +
+                            "     I chamnyetab Jehovah.",
+                    " Ki-iyeta eng' chamnyeng'ung'\n" +
+                            "Ribota kimnateng'ung'\n" +
+                            "Angen ale imutata\n" +
+                            "Agoi ait oldang'ung'.\n" +
+                            "     Kiabetoti eng' orennyu,\n" +
+                            "     Ko a lusat eng' chalwok.\n" +
+                            "     Ki-isorua eng' me-eng'ung':\n" +
+                            "     Asubun inye. nguno.",
+                    "O mie-indo ne kiyeta\n" +
+                            "Ak koriba kochulyo.\n" +
+                            "Kagonin muguleldanyu\n" +
+                            "Inet koyai mageng'ung'.\n" +
+                            "     I-ngonama eung'ung' ne kim\n" +
+                            "     Ak kondoiywa eng' oret\n" +
+                            "     Kot ait barak kipsengwet\n" +
+                            "     Ak kegeerge ak inye.",
+
+                    ), chorus = ""
+            )
+        )
+        hymns.add(
+            Hymn(
+                number = 11, title = "Betutab boiboiyet",
+                isFavourite = false,
+                stanzas = listOf(
+                    "Betutab boiboiyet ra,\n" +
+                            "Betutab Jehovah;\n" +
+                            "Mayae' chi ra boisiennyi,\n" +
+                            "Kiote Jehovah.\n" +
+                            "Kimuunyi rani kityo,\n" +
+                            "Kilosu Kiptaiyat.\n" +
+                            "Kiborjin bik Sirutik\n" +
+                            "Ng'alekab Jehovah.",
+                    "Betutab lapkeiyet ra\n" +
+                            "Ne bo Kiptaiyandet;\n" +
+                            "Kigase betunito\n" +
+                            "Ng'alekab Jehovah.\n" +
+                            "Kiba-e sobonwekyok\n" +
+                            "Makatiatab sobon,\n" +
+                            "Asi kegimegitu\n" +
+                            "Eng' sobennyo",
+                    "Kiting'ege yamda-et\n" +
+                            "Ak bikab Jehovah.\n" +
+                            "Kibwane si kilosu\n" +
+                            "Kamukta-indennyo.\n" +
+                            "Kilosu Kwanda rani\n" +
+                            "Ak Werinnyi Jesu,\n" +
+                            "Koboto Tamirmiriet.\n" +
+                            "Ee, kilosu tugul. \n"
+                ), chorus = ""
             )
         )
         hymns.add(
             Hymn(
                 number = 12,
 
-                title = "Yeiyo ne mi Jehova", isFavourite = false, stanzas = listOf(
-                    "Yeiyo ne mi Jehova...",
-                    "Kokwetab ng'wony chichi...",
-                    "Konyorunet ab keringet..."
+                title = "Kilosun eng' gongung'", isFavourite = false, stanzas = listOf(
+                    "Kichamin, Jehovah;\n" + "Kilosun eng' gong'ung'.\n" + "Kichame kegas ng'aleguk\n" + "Che 'ndochin kelyekyok.\n",
+                    "Nyolu mising' saet;\n" + "Mami ne kiroge,\n" + "Amu kituchini yoto\n" + "Kererwekyok bo met.",
+                    "Kichame kot mising'\n" + "Ng'olyotab sobondo\n" + "Ne ibu boiboiyet ne o\n" + "Ak kalyet agine.",
+                    "Kichame kot ketien\n" + "Kilosu Kiptaiyat\n" + "Ne rireneigei mising' kot\n" + "Agoi kebe barak.",
+                    "igiilech, Kiptaiyat,\n" + "Kechamin kot kosiir;\n" + "Agoi tun kegeerin kimni\n" + "Kilosun eng' gong'ung'."
                 ), chorus = "Chorus: Yeiyo ne mi kamukta"
             )
         )
@@ -172,11 +285,11 @@ object HymnRepository {
         return results
 
     }
+
     //add hymn to roomm database
     fun addHymn(hymn: Hymn) {
         hymns.add(hymn)
     }
-
 
 
 }

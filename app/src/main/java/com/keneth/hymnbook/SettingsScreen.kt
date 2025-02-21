cfea0b1325cf78410539fbf129e64183f4dba406
KENETH217 <kipyegonkeneth03@gmail.com>
2025-02-21 10:09:37 +0300

    add songs and navigation's

--- app/src/main/java/com/keneth/hymnbook/SettingsScreen.kt
+++ app/src/main/java/com/keneth/hymnbook/SettingsScreen.kt
@@ -1,11 +1,113 @@
 package com.keneth.hymnbook
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsScreen(navController: NavHostController){
-    Text(text = "Settings Screen")
+fun SettingsScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier,
 
+    ) {
+
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val currentRoute =
+        navController.currentBackStackEntry?.destination?.route ?: Screens.HymnScreen.route
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                AppDrawer(
+                    drawerState, scope,
+                    onItemClick = { route -> navController.navigate(route) }
+                )
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopBar(scope, drawerState, "Settings", onBottomSheetOpen = {
+
+                    showBottomSheet = true
+                    scope.launch { sheetState.show() }
+
+                })
+            },
+            bottomBar = { BottomNavBar(navController, currentRoute) }
+        ) { innerPadding ->
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+
+            ) {
+
+                Text(
+                    text = "Settings",
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                    ),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+
+
+                // Screen content
+
+                if (showBottomSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = {
+                            showBottomSheet = false
+                        },
+                        sheetState = sheetState
+                    ) {
+
+
+                        //bottom sheet content
+                        Text(
+                            text = "Bottom Sheet Content",
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                        //other meaning ful conent
+
+                    }
+                }
+
+            }
+        }
+    }
 }
+
